f2160f08620621458f951210d50549a0578ddf88
faith110 <chofaith05@gmail.com>
2025-11-07 15:18:26 +0100

    new

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+// Get the last modified date
+document.getElementById("lastModified").innerHTML= document.lastModified;
